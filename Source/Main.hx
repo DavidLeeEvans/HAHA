ed075f2b47565fe94862a5dcb02216d66d89819c
David Lee Evans <dle.evans@gmail.com>
2024-10-13 12:02:03 -0400

    wip

--- Source/Main.hx
+++ Source/Main.hx
@@ -3,15 +3,18 @@ package;
 import debug.FPSMEM.FPS_Mem;
 import openfl.display.FPS;
 import openfl.display.Sprite;
+import spine.Main;
 
 class Main extends Sprite {
 	public function new() {
 		super();
+		var second_main:Main = new Main();
 		var fps:FPS = new FPS(10, 100, 0xffffff);
 		//
 		addChild(fps);
 		var fps_mem:FPS_Mem = new FPS_Mem(10, 10, 0xffffff);
 		//
 		addChild(fps_mem);
+		addChild(second_main);
 	}
 }
