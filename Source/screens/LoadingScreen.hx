6ac6a38ffadf3c30ade8b6195395e0424bb78e2c
David Lee Evans <dle.evans@gmail.com>
2024-10-18 21:21:16 -0400

    wip

--- Source/screens/LoadingScreen.hx
+++ Source/screens/LoadingScreen.hx
@@ -1,18 +1,5 @@
 package screens;
 
-import openfl.display.Shape;
-import openfl.display.Sprite;
-
-import openfl.geom.Point;
-
-import openfl.text.TextField;
-import openfl.text.TextFormat;
-
-class LoadingScreen extends Sprite {
-		public function new() {
-		super();
-	}
-	// public function new(title:String, id:UInt, is_enabled:boolean) {
-	// 	super();
-	// }
+class LoadingScreen extends AbstractScreen {
+		
 }
