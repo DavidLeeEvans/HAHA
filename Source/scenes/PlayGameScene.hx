3a6fb284a359dc5bdfc691a10f5072f91ebf786f
David Lee Evans <dle.evans@gmail.com>
2024-11-16 18:55:34 -0500

    wip

--- Source/scenes/PlayGameScene.hx
+++ Source/scenes/PlayGameScene.hx
@@ -1,5 +1,6 @@
 package scenes;
 
+import starling.events.EnterFrameEvent;
 import starling.display.Sprite;
 import starling.animation.BezierEasing;
 import starling.animation.Transitions;
@@ -14,17 +15,42 @@ import openfl.Vector;
 import utils.MenuButton;
 
 @:keep class PlayGameScene extends Sprite {
+
+	private var _frameCount:Int;
+    private var _failCount:Int;
+    private var _started:Bool;
+    private var _frameTimes:Array<Float>;
+    private var _targetFps:Int;
+    private var _phase:Int;
+
 	public function new() {
 		super();
-
-
+		load_track(3); // TODO testing
+		load_characters();
+		load_good_opposition();
+		load_bad_opposition();
+		addEventListener(Event.ENTER_FRAME, onEnterFrame);
 	}
 
 	public override function dispose():Void {
 		super.dispose();
+		// dle TODO  removeEventListener(Event.ENTER_FRAME);
+		unload_assets();
 	}
+
     private function load_characters():Void {}
     private function load_track(level:Int):Void {}
     private function load_good_opposition():Void {}
     private function load_bad_opposition():Void {}
+	private function unload_assets():Void {}
+	
+
+	private function onEnterFrame(event:EnterFrameEvent, passedTime:Float):Void
+		{
+			if (!_started) return;
+		}
+	
 }
+
+
+
