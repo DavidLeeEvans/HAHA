f8018bac633107f1023779f4f9a579b636de28d9
David Lee Evans <dle.evans@gmail.com>
2024-12-28 19:07:27 -0500

    wip

---
+++ Source/ecs/characters/components/PositionComponent.hx
@@ -0,0 +1,19 @@
+package ecs.characters.components;
+import gasm.core.Component;
+import gasm.core.components.SpriteModelComponent;
+import gasm.core.enums.ComponentType;
+
+/**
+ * ...
+ * @author David Evans
+ */
+class PositionComponent extends Component {
+    public function new() {
+    }
+
+    override public function init() {
+    }
+
+    override public function update(delta:Float) {
+    }
+}
